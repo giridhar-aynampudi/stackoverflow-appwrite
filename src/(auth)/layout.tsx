0b52c6791826bfe59690f84678cb21f5457e4a0a
Giridhar <giridhar.aynampudi@rapidid.com>
2024-08-13 11:03:50 +1000

    Added zustand, shadcn, register page

---
+++ src/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import { useAuthStore } from "../store/auth";
+import React from "react";
+
+const Layout = ({children}: {children: React.ReactNode}) => {
+
+    const {session} = useAuthStore();
+    const router = useRouter();
+
+    React.useEffect(() => {
+        if(session) {
+            router.push("/")
+        }
+    }, [session, router])
+
+    if(session) return null;
+
+    return (
+        <div className="">
+            <div className="">{children}</div>
+        </div>
+    )
+
+}
+
+export default Layout
